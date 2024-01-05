ffda84c9e83dd91f19a311b5b187f3141cc18196
Retro <htoomyataung382@gmail.com>
2024-01-05 20:30:42 +0630

    test

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Nav from './Components/Navigation/Nav.tsx'
 import { UserProvider } from './hooks/useUser.tsx'
 import Profile from './Components/Profiles/Profile.tsx'
@@ -10,13 +10,13 @@ import Profile from './Components/Profiles/Profile.tsx'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <UserProvider>
-      <HashRouter>
+      <BrowserRouter>
         <Nav />
         <Routes>
           <Route path='/' element={<App />} />
           <Route path='/chat/profile' element={<Profile />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </UserProvider>
   </React.StrictMode>,
 )
