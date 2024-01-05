29f9e1958fe71369854f274312191f8e885a3707
Retro <htoomyataung382@gmail.com>
2024-01-05 20:21:22 +0630

    Fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,27 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import Nav from './Components/Navigation/Nav.tsx'
 import { UserProvider } from './hooks/useUser.tsx'
 import Profile from './Components/Profiles/Profile.tsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />
-  },
-  {
-    path: "/chat/profile",
-    element: <Profile />
-  }
-])
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <UserProvider>
-      <Nav />
-      <RouterProvider router={router} />
+      <HashRouter>
+        <Nav />
+        <Routes>
+          <Route path='/' element={<App />} />
+          <Route path='/chat/profile' element={<Profile />} />
+        </Routes>
+      </HashRouter>
     </UserProvider>
   </React.StrictMode>,
 )
